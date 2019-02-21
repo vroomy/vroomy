0a0fdf0eea59d14fd17f7b9eabd50d06cb8121c5
Josh Montoya <josh@montoya.io>
2019-02-21 13:08:40 -0800

    Remove non-needed funcs

--- service/utils.go
+++ service/utils.go
@@ -1,11 +1,7 @@
 package service
 
 import (
-	"fmt"
-	"net/url"
 	"os"
-	"os/exec"
-	"path"
 	"path/filepath"
 	"plugin"
 	"strings"
@@ -34,83 +30,6 @@ func getKeyFromRequestPath(root, requestPath string) (key string, err error) {
 	return
 }
 
-func getFilename(dir, key string) (filename string, err error) {
-	fmt.Println("Checking filename ", key)
-	switch {
-	case filepath.Ext(key) != "":
-		fmt.Println("SO")
-		filename = key
-		return
-
-	case isGitReference(key):
-		fmt.Println("Git reference!", key)
-		var pluginKey string
-		if pluginKey, err = getPluginKey(key); err != nil {
-			fmt.Println("What?", err)
-			return
-		}
-
-		if filename = filepath.Join(dir, pluginKey+".so"); doesPluginExist(filename) {
-			fmt.Println("Exists!", filename)
-			return
-		}
-
-		fmt.Println("Need to get from github", key)
-		return getFromGithub(dir, key)
-
-	default:
-		fmt.Println("Not supported?")
-	}
-
-	return
-}
-
-func getFromGithub(dir, gitURL string) (filename string, err error) {
-	var u *url.URL
-	if u, err = url.Parse("http://" + gitURL); err != nil {
-		return
-	}
-
-	spl := strings.Split(u.Path, "/")
-	if len(spl) > 2 {
-		spl = spl[:3]
-	}
-
-	u.Path = path.Join(spl...)
-
-	downloadURL := u.String()[7:]
-	fmt.Println("Download url", downloadURL)
-
-	goget := exec.Command("go", "get", downloadURL)
-	goget.Stdin = os.Stdin
-	goget.Stdout = os.Stdout
-	goget.Stderr = os.Stderr
-
-	fmt.Println("About to download", gitURL)
-	if err = goget.Run(); err != nil {
-		fmt.Println("Error", err)
-		return
-	}
-
-	homeDir := os.Getenv("HOME")
-	goDir := path.Join(homeDir, "go", "src", gitURL)
-	filename = filepath.Join(dir, filepath.Base(gitURL)+".so")
-
-	gobuild := exec.Command("go", "build", "--buildmode", "plugin", "-o", filename, goDir)
-	gobuild.Stdin = os.Stdin
-	gobuild.Stdout = os.Stdout
-	gobuild.Stderr = os.Stderr
-
-	fmt.Println("About to build", gitURL)
-	if err = gobuild.Run(); err != nil {
-		fmt.Println("Error", err)
-		return
-	}
-
-	fmt.Println("Yay")
-	return
-}
-
 func trimSlash(in string) (out string) {
 	if len(in) == 0 {
 		return
@@ -121,28 +40,6 @@ func trimSlash(in string) (out string) {
 	}
 
 	return in[:len(in)-1]
-}
-
-func doesPluginExist(filename string) (exists bool) {
-	fmt.Println("Check if plugin exists", filename)
-	return
-}
-
-func getPluginKey(filename string) (key string, err error) {
-	base := filepath.Base(filename)
-	spl := strings.Split(base, ".")
-	key = spl[0]
-	return
-}
-
-func getKeyFromGitURL(gitURL string) (key string, err error) {
-	var u *url.URL
-	if u, err = url.Parse("http://" + gitURL); err != nil {
-		return
-	}
-
-	key = filepath.Base(u.Path)
-	return
 }
 
 func getHandlerParts(handlerKey string) (key, handler string, args []string, err error) {
@@ -200,8 +97,14 @@ func newPluginHandler(p *plugins.Plugins, handlerKey string) (h httpserve.Handle
 	return
 }
 
-func isGitReference(handlerKey string) (ok bool) {
-	var err error
-	_, err = url.Parse("http://" + handlerKey)
-	return err == nil
+func initDir(loc string) (err error) {
+	if err = os.Mkdir(loc, 0744); err == nil {
+		return
+	}
+
+	if os.IsExist(err) {
+		return nil
+	}
+
+	return
 }
