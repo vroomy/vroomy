09afb88257677668a605bfab5ba95dc302b50bb5
Josh Montoya <josh@montoya.io>
2019-03-14 13:03:27 -0700

    Implement configuration includes

--- service/config.go
+++ service/config.go
@@ -1,6 +1,29 @@
 package service
 
+import (
+	"github.com/BurntSushi/toml"
+)
+
 const routeFmt = "{ HTTPPath: \"%s\", Target: \"%s\" Plugin Handler: \"%v\" }"
+
+func newConfig(loc string) (cfg *Config, err error) {
+	var c Config
+	if _, err = toml.DecodeFile(loc, &c); err != nil {
+		return
+	}
+
+	for _, include := range c.Include {
+		var icfg IncludeConfig
+		if _, err = toml.DecodeFile(include, &icfg); err != nil {
+			return
+		}
+
+		c.IncludeConfig.merge(&icfg)
+	}
+
+	cfg = &c
+	return
+}
 
 // Config is the configuration needed to initialize a new instance of Service
 type Config struct {
@@ -12,10 +35,8 @@ type Config struct {
 	Plugins []string `toml:"plugins"`
 	OnInit  []string `toml:"onInitialization"`
 
-	Groups []*Group `toml:"group"`
-
-	// Routes are the routes to listen for and serve
-	Routes []*Route `toml:"route"`
+	Include []string `toml:"include"`
+	IncludeConfig
 }
 
 func (c *Config) getGroup(name string) (g *Group, err error) {
@@ -34,4 +55,16 @@ func (c *Config) getGroup(name string) (g *Group, err error) {
 
 	err = ErrGroupNotFound
 	return
+}
+
+// IncludeConfig will include routes
+type IncludeConfig struct {
+	Groups []*Group `toml:"group"`
+	// Routes are the routes to listen for and serve
+	Routes []*Route `toml:"route"`
+}
+
+func (i *IncludeConfig) merge(merge *IncludeConfig) {
+	i.Groups = append(i.Groups, merge.Groups...)
+	i.Routes = append(i.Routes, merge.Routes...)
 }
