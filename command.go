37f9177da7b20d1e557a8b074009f1eb592542e5
Derek <d.halman@gmail.com>
2020-05-17 15:55:55 -0700

    gomu: Deploy local changes before incrementing version from v0.2.18

--- command.go
+++ command.go
@@ -13,11 +13,11 @@ func commandFromArgs() (cmd *parg.Command, err error) {
 	p = parg.New()
 
 	p.AddHandler("", runService, "Runs vroomy server.\n  Accepts flags specified in config.toml.\n  Use `vroomy` or `vroomy -<flag>`")
+	p.AddHandler("help", help, "Prints available commands and flags.\n  Use `vroomy help <command>` or `vroomy help <-flag>` to get more specific info.")
 
 	p.AddHandler("version", printVersion, "Prints current version of vroomy installation.\n  Use `vroomy version`")
-	p.AddHandler("upgrade", upgrade, "Upgrades vroomy installation.\n  Skips if version is up to date.\n  Use `vroomy upgrade`")
+	p.AddHandler("upgrade", upgrade, "Upgrades vroomy installation.\n  Skips if version is up to date.\n  Use `vroomy upgrade` or `vroomy upgrade branch`")
 
-	p.AddHandler("help", help, "Prints available commands and flags.\n  Use `vroomy help <command>` or `vroomy help <-flag>` to get more specific info.")
 	p.AddHandler("test", test, "Tests the currently checked out version of plugin(s).\n  Accepts filtered trailing args to target specific plugins.\n  Use `vpm test` for all plugins, or `vpm test <plugin> <plugin>`")
 
 	p.AddGlobalFlag(parg.Flag{
