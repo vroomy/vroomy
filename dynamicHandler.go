b2fee58c858ddcff8300f3be1e4022e66b145546
Derek <d.halman@gmail.com>
2020-05-17 01:56:05 -0700

    Whitespace

--- dynamicHandler.go
+++ dynamicHandler.go
@@ -23,25 +23,27 @@ func (dh dynamicHandler) handleDynamicCmd(cmd *parg.Command) (err error) {
 		handleError(err)
 	}
 
-	out.Notification("Initialized plugins successfully!")
+	out.Notificationf("Handling dynamic command: %s", dh.handler)
 
 	// Parse command handler
-	out.Notificationf("Handling: %s", dh.handler)
-
 	comps := strings.Split(dh.handler, ".")
 	if len(comps) != 2 {
 		return fmt.Errorf("error: unable to parse plugin and handler format <plugin>.<handler> from: %s", dh.handler)
 	}
+
+	// Check plugin
 	p, err := svc.Plugins.Get(comps[0])
 	if err != nil {
 		return
 	}
 
+	// Check handler method
 	sym, err := p.Lookup(comps[1])
 	if err != nil {
 		return
 	}
 
+	// Confirm plugin has supported handler syntax
 	var handlerErr error
 	switch fn := sym.(type) {
 	case func(flags, env map[string]string) error:
@@ -63,19 +65,16 @@ func (dh dynamicHandler) handleDynamicCmd(cmd *parg.Command) (err error) {
 	out.Notification("Closing...")
 
 	if err = svc.Close(); err != nil {
-		out.Error("Close test failed :(")
-
 		err = fmt.Errorf("error encountered while closing %s: %v", serviceName, err)
-		handleError(err)
+		return
 	}
 
 	out.Notification("Closed plugins successfully!")
 
-	if handlerErr == nil {
-		out.Successf("Executed %s!", cmd.Action)
-	} else {
-		handleError(handlerErr)
+	if handlerErr != nil {
+		return handlerErr
 	}
 
+	out.Successf("Executed %s!", cmd.Action)
 	return
 }
