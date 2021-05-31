f036fec29d33087c935ca0bb818bfd960eaaa8bb
Josh Montoya <josh@montoya.io>
2021-05-31 15:29:01 -0700

    convert vroomy to a library

--- utils.go
+++ utils.go
@@ -1,192 +1,23 @@
-package main
+package vroomy
 
 import (
 	"fmt"
 	"os"
-	"strconv"
+	"reflect"
 	"strings"
-	"time"
 
-	"github.com/gdbu/scribe"
-	flag "github.com/hatchify/parg"
-	"github.com/vroomy/config"
+	"github.com/hatchify/errors"
+	"github.com/vroomy/common"
+	"github.com/vroomy/plugins"
 )
 
-// Load the action and config environment
-func setupRuntime() (cmd *flag.Command) {
-	out = scribe.New("vroomy")
+const (
+	// ErrExpectedEndParen is returned when an ending parenthesis is missing
+	ErrExpectedEndParen = errors.Error("expected ending parenthesis")
+)
 
-	// Load config location
-	configLocation := os.Getenv("VROOMY_CONFIG")
-	if len(configLocation) == 0 {
-		configLocation = DefaultConfigLocation
-	}
-
-	// Parse config
-	var cfgErr error
-	cfg, cfgErr = config.NewConfig(configLocation)
-
-	// Load command (apply config if available)
-	var err error
-	cmd, err = commandFromArgs()
-	if err != nil {
-		showHelp(nil)
-		handleError(err)
-		return
-	}
-
-	if customConfig := cmd.StringFrom("config"); customConfig != "" {
-		// Parse config
-		cfgErr = nil
-		cfg, cfgErr = config.NewConfig(customConfig)
-	}
-
-	switch cmd.Action {
-	case "version", "upgrade":
-		// Global actions
-	default:
-		if cfgErr != nil {
-			out.Warning("Warning :: No config set.")
-
-			if cmd.Action == "help" {
-				// We can ignore config errors if we're asking for help. Use default
-				cfg = &config.Config{Name: "vroomy service"}
-			} else {
-				// Config is required
-				handleError(cfgErr)
-			}
-		}
-
-		// Parse flags into config
-		if err = parseConfigFlagsFrom(cmd); err != nil {
-			showHelp(cmd)
-
-			handleError(err)
-		}
-
-		var dataDir = cmd.StringFrom("dataDir")
-		if len(dataDir) > 0 {
-			// Override config
-			cfg.Environment["dataDir"] = dataDir
-		}
-	}
-
-	return
-}
-
-// Starts server
-func initService() (err error) {
-	out.Notificationf("Starting %s...", cfg.Name)
-
-	if _, ok := cfg.Environment["dataDir"]; !ok {
-		// Default if not set elsewhere
-		cfg.Environment["dataDir"] = "data"
-	}
-	out.Notificationf("Setting data directory: %s", cfg.Environment["dataDir"])
-
-	if svc, err = New(cfg); err != nil {
-		err = fmt.Errorf("error encountered while initializing service: %v", err)
-		return
-	}
-
-	return
-}
-
-//
-func listen() {
-	var err error
-	if err = svc.Listen(); err == nil {
-		return
-	}
-
-	err = fmt.Errorf("error encountered while attempting to listen to HTTP: %v", err)
-	clsr.Close(err)
-}
-
-func notifyOfListening() {
-	time.Sleep(time.Millisecond * 300)
-	msg := getListeningMessage(svc.Port(), svc.TLSPort())
-	out.Successf("HTTP is now listening on %s", msg)
-}
-
-func getListeningMessage(port, tlsPort uint16) (msg string) {
-	switch {
-	case port > 0 && tlsPort > 0:
-		msg = fmt.Sprintf("ports %d (HTTP) and %d (HTTPS)", port, tlsPort)
-	case port > 0:
-		msg = fmt.Sprintf("port %d (HTTP)", port)
-	case tlsPort > 0:
-		msg = fmt.Sprintf("port %d (HTTPS)", tlsPort)
-	}
-
-	return
-}
-
-func handleError(err error) {
-	out.Errorf("Fatal error encountered: %v", err)
-	os.Exit(1)
-}
-
-// Convert command flags to flag entities, handle defaults
-func parseConfigFlagsFrom(cmd *flag.Command) (err error) {
-	cfg.Flags = map[string]string{}
-
-	// Set requires first, override if provided elsewhere
-	for _, c := range cfg.CommandEntries {
-		if c.Name == cmd.Action {
-			// We're running this command
-			if len(c.Require) != 0 {
-				cfg.Flags["require"] = c.Require
-			}
-
-			break
-		}
-	}
-
-	// Add default values
-	cmdFlags := cmd.Flags
-	for _, entry := range cfg.FlagEntries {
-		if entry.DefaultValue != "" {
-			if _, ok := cmd.Flags[entry.Name]; !ok {
-				// Set default value
-				cfg.Flags[entry.Name] = entry.DefaultValue
-			}
-		}
-	}
-
-	// Parse flags, override defaults
-	for name, f := range cmdFlags {
-		switch f.Type {
-		case flag.DEFAULT:
-			cfg.Flags[name] = cmd.StringFrom(f.Name)
-		case flag.STRINGS:
-			cfg.Flags[name] = strings.Join(cmd.StringsFrom(f.Name), " ")
-		case flag.BOOL:
-			if cmd.BoolFrom(f.Name) {
-				cfg.Flags[name] = "true"
-			} else {
-				cfg.Flags[name] = "false"
-			}
-		case flag.INT:
-			cfg.Flags[name] = strconv.Itoa(cmd.IntFrom(f.Name))
-
-		default:
-			// Needs argument unless asking for usage
-			err = fmt.Errorf("error: %s flag received unexpected argument type: unable to parse \"%+v\"", f.Name, f.Type)
-			return
-		}
-
-		if len(cfg.Flags[name]) == 0 {
-			if cmd.Action != "help" {
-				// Needs argument unless asking for usage
-				err = fmt.Errorf("error: \"-%s\" flag expects %s: got \"%+v\"", f.Name, f.Type.Expects(), f.Value)
-				return
-			}
-			return
-		}
-	}
-
-	return
+type listener interface {
+	Listen(port uint16) error
 }
 
 func initDir(loc string) (err error) {
@@ -205,6 +36,69 @@ func initDir(loc string) (err error) {
 	return
 }
 
-type listener interface {
-	Listen(port uint16) error
+func getHandlerParts(handlerKey string) (key, handler string, args []string, err error) {
+	spl := strings.SplitN(handlerKey, ".", 2)
+	if len(spl) != 2 {
+		err = fmt.Errorf("expected key and handler, received \"%s\"", handlerKey)
+		return
+	}
+
+	key = spl[0]
+	handler = spl[1]
+
+	spl = strings.Split(handler, "(")
+	if len(spl) == 1 {
+		return
+	}
+
+	handler = spl[0]
+	argsStr := spl[1]
+
+	if argsStr[len(argsStr)-1] != ')' {
+		err = ErrExpectedEndParen
+		return
+	}
+
+	argsStr = argsStr[:len(argsStr)-1]
+	args = strings.Split(argsStr, ",")
+	return
+}
+
+func getHandler(handlerKey string) (h common.Handler, err error) {
+	var (
+		key     string
+		handler string
+		args    []string
+	)
+
+	if key, handler, args, err = getHandlerParts(handlerKey); err != nil {
+		return
+	}
+
+	var p plugins.Plugin
+	if p, err = plugins.Get(key); err != nil {
+		return
+	}
+
+	reflected := reflect.ValueOf(p).MethodByName(handler)
+	if reflected.Kind() == reflect.Invalid {
+		err = fmt.Errorf("method of <%s> not found within plugin <%s>", handler, key)
+		return
+	}
+
+	toAssert := reflected.Interface()
+
+	switch val := toAssert.(type) {
+	case func(common.Context):
+		h = val
+		return
+
+	case func(args ...string) common.Handler:
+		h = val(args...)
+		return
+
+	default:
+		err = fmt.Errorf("invalid handler type, expected common.Handler and received %T", val)
+		return
+	}
 }
